978f1b08f3afdac4e6445b90d3adadb0c27756c8
pannellr <richard.pannell@gmail.com>
2016-03-07 09:35:38 -0400

    cleaning up viz to share

--- scripts.js
+++ scripts.js
@@ -2,33 +2,8 @@
  * Created by pannellr on 2016-03-05.
  */
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
 
-var formatPercent = d3.format(".0%");
-
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1, 1);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(formatPercent);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+//================rickey map listings by neighborhood
 
 var neighbourhoodFrequncy = {};
 
@@ -40,78 +15,6 @@ _.each(listings, function(listing) {
         };
     } else {
         neighbourhoodFrequncy[listing.neighbourhood]['frequency']++;
-    }
-});
-
-d3.json("./vancouver_data/listings.json", function(error, l) {
-
-
-
-    //convert to array
-    var frequencyList = Object.keys(neighbourhoodFrequncy).map(function (key) {return neighbourhoodFrequncy[key]});
-
-    frequencyList.forEach(function(d) {
-        d.frequency = +d.frequency;
-    });
-
-    x.domain(frequencyList.map(function(d) { return d.name; }));
-    y.domain([0, d3.max(frequencyList, function(d) { return d.frequency; })]);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .style("text-anchor", "end");
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
-
-    svg.selectAll(".bar")
-        .data(frequencyList)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.name); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.frequency); })
-        .attr("height", function(d) { return height - y(d.frequency); });
-
-    d3.select("input").on("change", change);
-
-    var sortTimeout = setTimeout(function() {
-        d3.select("input").property("checked", true).each(change);
-    }, 2000);
-
-    function change() {
-        clearTimeout(sortTimeout);
-
-        // Copy-on-write since tweens are evaluated after a delay.
-        var x0 = x.domain(frequencyList.sort(this.checked
-                ? function(a, b) { return b.frequency - a.frequency; }
-                : function(a, b) { return d3.ascending(a.name, b.name); })
-            .map(function(d) { return d.name; }))
-            .copy();
-
-        svg.selectAll(".bar")
-            .sort(function(a, b) { return x0(a.name) - x0(b.name); });
-
-        var transition = svg.transition().duration(750),
-            delay = function(d, i) { return i * 50; };
-
-        transition.selectAll(".bar")
-            .delay(delay)
-            .attr("x", function(d) { return x0(d.name); });
-
-        transition.select(".x.axis")
-            .call(xAxis)
-            .selectAll("g")
-            .delay(delay);
     }
 });
 
@@ -152,3 +55,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 }).addTo(map);
 
 L.geoJson(neighbourhoods, {style: style}).addTo(map);
+
+//=====================end rickey===================
